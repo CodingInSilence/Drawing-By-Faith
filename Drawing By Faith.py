4fcbed8fbc614a78e62ffefb55a5a05aa1f2667b
CodingInSilence <150291131+CodingInSilence@users.noreply.github.com>
2023-12-07 21:49:14 -0800

    Add files via upload

---
+++ Drawing By Faith.py
@@ -0,0 +1,18 @@
+
+import turtle
+s = turtle.getscreen()
+s.setup(width=1000, height=1000)
+t = turtle
+input("What is your name")
+
+t.penup()
+t.goto(0,-400)
+t.pendown()
+t.speed(4)
+
+t.pensize(5)
+# t.left(90)
+# t.forward(380)
+# t.right(90)
+# t.circle(40, 22.5)
+t.done()
